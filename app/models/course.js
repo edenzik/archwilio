02227be9a8a19abcce91f5fe996bbe926bca0038
Josh Silverman <joshs.silverman@gmail.com>
2015-11-05 13:50:10 -0500

    Remove the now ignored directory node_modules

---
+++ app/models/course.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  // var User = sequelize.define("User", {
+  //   username: DataTypes.STRING
+  // }, {
+  //   classMethods: {
+  //     associate: function(models) {
+  //       User.hasMany(models.Task)
+  //     }
+  //   }
+  // });
+
+  // var Course = sequelize.define('Course', {
+  //   name: DataTypes.STRING
+  // });
+
+  return Course;
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,22 @@
+var models  = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
   res.render('index', { title: 'Express' });
+
+}).get('/courses', function(req, res) {
+  
+  models.Course.findAll({
+    include: [ models.Course ]
+  }).then(function(users) {
+    res.render('index', {
+      title: 'Course',
+      course: courses
+    });
+  });
 });
 
 module.exports = router;
