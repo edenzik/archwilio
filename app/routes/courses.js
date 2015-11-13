00202844f99bd9ec88e84c03e3560a975ac1b469
Josh Silverman <joshs.silverman@gmail.com>
2015-11-13 16:10:37 -0500

    courses endpoint working

--- app/routes/courses.js
+++ app/routes/courses.js
@@ -2,14 +2,12 @@ var express = require('express');
 var models  = require('../models');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET courses listing. */
 router.get('/', function(req, res) {
   models.Course.findAll({}).then(function(courses) {
-    res.render('index', {
-      title: 'Course',
-      course: '1'
+    res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(courses));
     });
-  });
 });
 
 module.exports = router;
