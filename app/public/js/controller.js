ba77b395725bfa680c1f953a30527b29848a0597
JackZhongTJ <zhongtianjie2014@gmail.com>
2015-11-27 18:01:04 -0500

    pass rating argument to train function

--- app/public/js/controller.js
+++ app/public/js/controller.js
@@ -32,10 +32,33 @@ ClassPathExploration.controller('CourseCtrl', function ($scope, $http, $modal, $
 ClassPathExploration.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, $http, course) {
   $scope.course = course;
 
-  $scope.train = function(rating) {
+  $scope.train = function() {
+    /*get element value in modal */
+    var rating;
+    if (document.getElementById('yes').checked) {
+      var rating = document.getElementById('yes').value;
+    }
+    else if(document.getElementById('no').checked){
+      var rating = document.getElementById('no').value;
+    }
+    
+    var positive_reason_1 = document.getElementById('positive_reason_1').checked;
+    var positive_reason_2 = document.getElementById('positive_reason_2').checked;
+    var positive_reason_3 = document.getElementById('positive_reason_3').checked;
+    var negative_reason_1 = document.getElementById('negative_reason_1').checked;
+    var negative_reason_2 = document.getElementById('negative_reason_2').checked;
+    var negative_reason_3 = document.getElementById('negative_reason_3').checked;
+    /* */
+
     data = {
       instance_id: $scope.course.instance_id,
-      rating: $scope.rating
+      rating: $scope.rating,
+      positive_reason_1: $scope.positive_reason_1,
+      positive_reason_2: $scope.positive_reason_2,
+      positive_reason_3: $scope.positive_reason_3,
+      negative_reason_1: $scope.negative_reason_1,
+      negative_reason_2: $scope.negative_reason_2,
+      negative_reason_3: $scope.negative_reason_3
     };
 
     $http.post('http://localhost:3000/courses/train', data).success(function() {});
