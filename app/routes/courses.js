dada3d357b211fbc024f0903e853b240c737112e
Josh Silverman <joshs.silverman@gmail.com>
2015-11-29 10:59:41 -0500

    prep for post to train service

--- app/routes/courses.js
+++ app/routes/courses.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var models  = require('../models');
 var router = express.Router();
-var sequelize = models.sequelize
+var sequelize = models.sequelize;
+var http = require('http');
 
 router.get('/', function(req, res) {
   models.course.findAll(
@@ -27,6 +28,19 @@ router.get('/', function(req, res) {
 });
 
 router.post('/train', function(req, res) {
+  function train(response) {
+    var str = '';
+
+    response.on('data', function (chunk) {
+      str += chunk;
+      // console.log(chunk);
+    });
+
+    response.on('end', function () {
+      console.log(str);
+    });
+  }
+
   var instance_id = req.body.instance_id,
       rating = 0.8;
 
@@ -36,13 +50,18 @@ router.post('/train', function(req, res) {
     }).then(function(course) {
       console.log(instance_id);
       console.log(rating);
-      
       console.log(course.dataValues.description);
       console.log(course.dataValues.code);
       console.log(course.dataValues.name);
+
+      var options = {
+        host: 'localhost',
+        port: '8080',
+        path: '/'
+      };
+
+      http.request(options, train).end();
     });
-
-
 });
 
 module.exports = router;
