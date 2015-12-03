48bd968d6ec45ac333143d44385a01ebe9e8ceed
Kahlil Oppenheimer <kahliloppenheimer@gmail.com>
2015-12-03 18:50:24 -0500

    Added example courses demo

---
+++ app/public/js/example_courses.js
@@ -0,0 +1,21 @@
+var nodes = [
+  {id: 1,  label: '11a', value: 22},
+  {id: 2,  label: '12b', value: 100},
+  {id: 3,  label: '21a', value: 22},
+  {id: 4,  label: '29a', value: 22},
+  {id: 5,  label: '21b', value: 26},
+  {id: 6,  label: '127b', value: 22},
+  {id: 7,  label: '21a', value: 24},
+  {id: 8,  label: '29a', value: 34}
+];
+
+var edges = [
+  {from: 1, to: 2},
+  {from: 1, to: 3},
+  {from: 1, to: 6},
+  {from: 1, to: 5},
+  {from: 1, to: 8},
+  {from: 1, to: 7},
+  {from: 4, to: 6},
+  {from: 2, to: 3}
+];
