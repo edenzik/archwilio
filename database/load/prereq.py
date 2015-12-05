5584d0ce24881bea6d883ad52f2b4d9147ee82be
edenzik <edenzik@gmail.com>
2015-12-05 00:18:56 -0500

    added working script

--- database/load/prereq.py
+++ database/load/prereq.py
@@ -11,8 +11,8 @@ cur = conn.cursor()
 cur.execute("DROP TABLE IF EXISTS prereq;")
 cur.execute("DROP TABLE IF EXISTS coreq;")
 
-cur.execute("CREATE TABLE prereq (id text, prereq_name text);")
-cur.execute("CREATE TABLE coreq (id text, coreq_name text);")
+cur.execute("CREATE TEMPORARY TABLE temp_prereq (id text, prereq_name text);")
+cur.execute("CREATE TEMPORARY TABLE temp_coreq (id text, coreq_name text);")
 
 # Execute a command: this creates a new table
 cur.execute("SELECT id, description FROM course")
@@ -25,14 +25,13 @@ rows = cur.fetchall()
 re.match
 
 for row in rows:
-	prereq_match = re.search('(Prerequisites:[^\.]+)', row[1])
+	prereq_match = re.search('(Prerequisites?:[^\.]+)', row[1])
 	if prereq_match:
-		#print prereq_match.group(1)
 		prereq_string = prereq_match.group(1)
 		classes = re.finditer('([A-Z]+ \d+[a-z])',prereq_string)
 		if classes:
 			for prereq in classes:
-				cur.execute("INSERT INTO prereq(id,prereq_name) VALUES ('%s','%s');" % (row[0].split("-")[1], prereq.group(1)))
+				cur.execute("INSERT INTO temp_prereq(id,prereq_name) VALUES ('%s','%s');" % (row[0].split("-")[1], prereq.group(1)))
 	coreq_match = re.search('(Corequisites:[^\.]+)', row[1])
 	if coreq_match:
 		#print prereq_match.group(1)
@@ -40,13 +39,17 @@ for row in rows:
 		classes = re.finditer('([A-Z]+ \d+[a-z])',coreq_string)
 		if classes:
 			for coreq in classes:
-				cur.execute("INSERT INTO coreq(id,coreq_name) VALUES ('%s','%s');" % (row[0].split("-")[1], coreq.group(1)))
+				cur.execute("INSERT INTO temp_coreq(id,coreq_name) VALUES ('%s','%s');" % (row[0].split("-")[1], coreq.group(1)))
 	#print row
 # Query the database and obtain data as Python objects
 
 # Make the changes to the database persistent
-conn.commit()
 
+cur.execute("CREATE TABLE prereq AS (SELECT temp_prereq.id AS course_id, to_course.instance_id AS prereq_id FROM temp_prereq, course AS to_course  WHERE upper(temp_prereq.prereq_name) = to_course.code GROUP BY temp_prereq.id,to_course.instance_id);")
+
+cur.execute("CREATE TABLE coreq AS (SELECT temp_coreq.id AS course_id, to_course.instance_id AS coreq_id FROM temp_coreq, course AS to_course  WHERE upper(temp_coreq.coreq_name) = to_course.code GROUP BY temp_coreq.id,to_course.instance_id);")
+
+conn.commit()
 print "LOADED all pre/co requisites into the database"
 
 # Close communication with the database
